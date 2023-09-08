67582bacbcffd2fd42e5a9de2c9097f8d47441d4
D-realsamdev <ajakayedamilola7@gmail.com>
2023-09-08 06:12:16 +0100

    update

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import  blogSlice  from './features/blogSlice';
+// import  blogSlice  from './features/blogSlice';
 // import AuthProvider from '../hooks/useFirebase';
 
 export const store = configureStore({
